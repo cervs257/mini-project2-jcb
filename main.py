c828f5348dd20a2c732433090b25db176072d3be
cervs257 <33461065+cervs257@users.noreply.github.com>
2023-09-08 14:40:57 -0400

    Update main.py
    inital get ccy script

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-def isPrime(n):
-    if n<=1:
-        return False
-    elif n==2:
-        return True
-    else:   
-        for i in range(2,n):
-            if n%i == 0:
-                return False
-        return True
+import yfinance as yf
+import pandas as pd
+import numpy as np
 
-        
+def historic_ccy():
+    # Define the ticker symbol for USD/MXN
+    ticker_symbol = "USDMXN"
+    
+    # Define the date range for the past month
+    end_date = datetime.date.today()
+    start_date = end_date - datetime.timedelta(days=30)
+    
+    # Fetch the data from Yahoo Finance
+    usdmxn_data = yf.download(ticker_symbol, start=start_date, end=end_date)
+    
+    # Extract the closing prices
+    closing_prices = usdmxn_data['Close']
+    
+    # Print the closing prices for the past month
+    print(closing_prices)
